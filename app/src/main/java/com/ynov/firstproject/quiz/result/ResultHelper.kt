b0c723aa12a0f3da3df9dae1dac1dabb8143d28b
Vesval <clara.vesval@ynov.com>
2023-01-27 17:02:20 +0100

    Ultime Answer with comment

--- app/src/main/java/com/ynov/firstproject/quiz/result/ResultHelper.kt
+++ app/src/main/java/com/ynov/firstproject/quiz/result/ResultHelper.kt
@@ -9,9 +9,9 @@ class ResultHelper {
         fun getResultFromScore(score: Int) : Pair<Int,Int> {
             return when(score){
                 in 11..26 -> Pair(R.drawable.griffondor, R.string.griffondor)
-                in 27..41 -> Pair(R.drawable.serdaigle, R.string.serdaigle)
-                in 42..56 -> Pair(R.drawable.poufsouffle, R.string.poufsouffle)
-                in 57..70 -> Pair(R.drawable.serpentard, R.string.serpentard)
+                in 27..31 -> Pair(R.drawable.serdaigle, R.string.serdaigle)
+                in 32..46 -> Pair(R.drawable.poufsouffle, R.string.poufsouffle)
+                in 47..60 -> Pair(R.drawable.serpentard, R.string.serpentard)
                 else -> Pair(R.drawable.moldus, R.string.moldu)
             }
         }

9b3be017b35d34bdf1b3196158452874b33aef92
Bastien Brunet <b.brunet@itso.fr>
2023-01-27 17:16:34 +0100

    Name intent

--- app/src/main/java/com/ynov/firstproject/quiz/PersonalInformationActivity.kt
+++ app/src/main/java/com/ynov/firstproject/quiz/PersonalInformationActivity.kt
@@ -20,7 +20,7 @@ class PersonalInformationActivity : AppCompatActivity() {
         val textInput = findViewById<TextView>(R.id.inputName)
         if (textInput.text.isNotEmpty() && textInput.text.isNotBlank() )
         {
-            intent.putExtra("NAME",textInput.text)
+            intent.putExtra("NAME",textInput.text.toString())
             startActivity(intent)
         } else {
             Toast.makeText(applicationContext, R.string.answer_mandatory, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/ynov/firstproject/quiz/QuizActivity.kt
+++ app/src/main/java/com/ynov/firstproject/quiz/QuizActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -26,6 +27,7 @@ class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        name = intent.getStringExtra("NAME") as String
         createQuiz()
     }
 
